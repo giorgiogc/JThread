c2a5f4229b558b5e1020658ca891a7cf493bde94
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2017-03-10 08:38:18 +0100

    Changed date in copyright notices

--- src/win32/jmutex.cpp
+++ src/win32/jmutex.cpp
@@ -3,7 +3,7 @@
     This file is a part of the JThread package, which contains some object-
     oriented thread wrappers for different thread implementations.
 
-    Copyright (c) 2000-2011  Jori Liesenborgs (jori.liesenborgs@gmail.com)
+    Copyright (c) 2000-2017  Jori Liesenborgs (jori.liesenborgs@gmail.com)
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
